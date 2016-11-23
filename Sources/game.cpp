03e9da00dc5f4c0cf296cf71fff18386115f142c
Marcus Östling <maostli@kth.se>
2016-11-23 09:33:11 +0100

    Game uses renderer :zap:

--- Sources/game.cpp
+++ Sources/game.cpp
@@ -5,13 +5,36 @@
    @version 0
    */
 
-
-#include <SFML/Graphics.hpp>
-#include <vector>
-#include "entity.hpp"
 #include "player.hpp"
 #include "game.hpp"
 
 Game::Game(int x, int y, std::string title) {
     sf::RenderWindow window(sf::VideoMode(x, y), title);
+    
+    Renderer renderer(&window, &entities);
+
+    GameLoop(&renderer, &window);
+}
+
+Game::~Game() {
+    for(Entity* e : entities) {
+        delete e;
+    }
+}
+
+void Game::GameLoop(Renderer* renderer, sf::RenderWindow* window) {
+    while(window->isOpen()) {
+        
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+        
+        renderer->Render();
+    }
+    
+    //delete renderer;
+    //delete window;
 }
--- Sources/game.hpp
+++ Sources/game.hpp
@@ -1,11 +1,19 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include <SFML/Graphics.hpp>
 #include <string>
+#include <vector>
+#include "entity.hpp"
+#include "renderer.hpp"
 
 class Game {
+private:
+    std::vector<Entity*> entities;
 public:
     Game(int x, int y, std::string title);
+    virtual ~Game();
+    void GameLoop(Renderer* renderer, sf::RenderWindow* window);
 };
 
 #endif
